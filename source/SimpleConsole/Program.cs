074e3d7680c8784b9fcb1328ebb56e1701b1505e
walt-ritscher <waltritscher@outlook.com>
2022-03-16 13:03:41 -0700

    Add example project

---
+++ source/SimpleConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class SimpleConsole {
+  static void Main(string[] args) {
+
+    Console.WriteLine("Let's examine some .NET Attributes!");
+  }
+}
+

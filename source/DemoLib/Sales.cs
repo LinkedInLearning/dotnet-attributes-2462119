bd1ab620352ee14fd9cc71d0ef3978e89e59aab3
walt-ritscher <waltritscher@outlook.com>
2022-03-22 16:21:56 -0700

    Add sample code

---
+++ source/DemoLib/Sales.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoLib {
+  [Obsolete]
+  public class Sales {
+
+    [Obsolete ("Use the IsValidSale method instead")]
+    public bool GetCustomerApproval(decimal salesAmount) {
+      if (salesAmount > 500)
+      {
+        return false;
+      }
+      else
+      { 
+        return true;
+      }
+    
+    }
+  }
+}
--- source/SimpleConsole/Program.cs
+++ source/SimpleConsole/Program.cs
@@ -3,6 +3,13 @@ public class SimpleConsole {
   static void Main(string[] args) {
 
     Console.WriteLine("Let's examine some .NET Attributes!");
+
+    var sales = new DemoLib.Sales();
+
+    bool approved = sales.GetCustomerApproval(350M);
+
+    Console.WriteLine($"Sale is approved");
+
   }
 }
 

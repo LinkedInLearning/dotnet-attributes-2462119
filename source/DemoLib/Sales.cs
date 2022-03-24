d592d00f3e9959fca3835cf353646888a55665f8
walt-instructor <staffauthor@outlook.com>
2022-03-23 21:55:37 -0700

    Add more attributes

--- source/DemoLib/Sales.cs
+++ source/DemoLib/Sales.cs
@@ -1,11 +1,20 @@
 ﻿using System.Runtime.CompilerServices;
 namespace DemoLib {
 
-
+  [Obsolete][Serializable]
   public class Sales { }
 
+
+  [Obsolete]
+  [Serializable]
   public class BalanceSheet { }
+
+  [Obsolete]
+  [Serializable]
   public class Tax { }
+  
+  [Obsolete]
+  [Serializable]
   public class Witholding { }
 
   }

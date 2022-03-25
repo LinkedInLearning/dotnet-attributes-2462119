897721ae4c3436008d0fe9a92f96b313d9d776d0
walt-instructor <staffauthor@outlook.com>
2022-03-24 20:25:34 -0700

    Create starting example

---
+++ source/JsonSmasher.Core/JsonSmasher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smasher.Core {
+  public static class JsonSmasher {
+
+    public static string MakeJson<T>(T theClass) where T : class {
+      
+      Type type = typeof(T);
+
+      // Get the class properties not include inherited properties
+      var classProps = type.GetProperties(BindingFlags.Public 
+                                          | BindingFlags.NonPublic 
+                                          | BindingFlags.Static   
+                                          | BindingFlags.Instance);
+		
+
+    // find the attribute...
+    // reverse the sort order when requested
+
+			StringBuilder sb = new StringBuilder();
+      sb.Append('{');
+      foreach (var prop in classProps)
+      {
+        sb.AppendLine($"   \"{prop.Name}\" : \"{prop.GetValue(theClass)}\",");
+
+      }
+
+      sb.Append('}');
+
+      return sb.ToString();
+    }
+  }
+}
---
+++ source/ShowJsonApp/CollectableCard.cs
@@ -0,0 +1,37 @@
+﻿using Smasher.Core;
+
+namespace DataLib {
+
+  
+  public class CollectableCard {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Slogan { get; set; }
+
+    public TeamNames TeamName { get; set; }
+
+    public string? Description { get; set; }
+    public decimal CatalogPrice { get; set; }
+    public decimal BidPrice { get; set; }
+    public int PopularityIndex { get; set; }
+  }
+}
+
+public enum CardFamily {
+  Monsters,
+  Aliens,
+  Robots,
+  Animals
+}
+
+public enum TeamNames {
+  BlueShadows,
+  ThunderHeads,
+  SpookTones,
+  Crashmasters,
+  Mavericks,
+  LaserPhasers,
+  Helios,
+  Farsiders,
+  SuperNovas
+}

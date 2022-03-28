8791c1fc444072b7f137ac62f4829a97691d6d88
walt-ritscher <waltritscher@outlook.com>
2022-03-28 16:49:51 -0700

    Add IpAddress control

---
+++ source/WpfControlLib/IpAddress.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Controls;
+using System.ComponentModel;
+namespace WpfControlLib
+{
+
+  public partial class IpAddress : UserControl
+  {
+    public IpAddress()
+    {
+      InitializeComponent();
+    }
+
+ 
+    public String CurrentIP
+    {
+      get
+      {
+        String temp = String.Format("{0}.{1}.{2}.{3}", ipPart1TextBox.Text, ipPart2TextBox.Text, ipPart3TextBox.Text, ipPart4TextBox.Text);
+        return temp;
+      }
+      set
+      {
+        String[] parts = value.Split('.');
+        if (parts.Length != 4)
+        {
+          throw new ArgumentOutOfRangeException("CurrentIP must have four values");
+        }
+
+        ipPart1TextBox.Text = parts[0];
+        ipPart2TextBox.Text = parts[1];
+        ipPart3TextBox.Text = parts[2];
+        ipPart4TextBox.Text = parts[3];
+      }
+    }
+
+		
+  }
+}
